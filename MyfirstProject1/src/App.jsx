f4d2343954447920f672dd6678f32b702ad48c02
Milan <meelantamang0@gmail.com>
2024-05-27 18:05:16 +0545

    sakkyo

---
+++ MyfirstProject1/src/App.jsx
@@ -0,0 +1,14 @@
+
+import "./App.css"
+import Navigation from "./components/Navigation";
+import XpulseBike from "./components/Xpulse";
+const App = () => {
+  return ( <div>
+     
+            < Navigation />
+            <XpulseBike />
+  </div>
+  )
+}
+
+export default App
---
+++ MyfirstProject1/src/components/Xpulse.jsx
@@ -0,0 +1,31 @@
+const SbDunk = () => {
+    return (
+    <main className="hero container">
+        <div className="hero-content">
+            <h1>
+                SUPER FINEST AFFORDABLE SHOES IN THE WORLD
+            </h1>
+            <p>
+            Nike shoes are renowned for their innovative design, superior comfort, and cutting-edge technology. With a wide range of options catering to various sports and lifestyles, Nike continually sets the standard in athletic footwear.
+            </p>
+
+            <div className="nike-button">
+                <button> Shop Now</button>
+                <button className="secondary-btn">Category</button>
+            </div>
+            <div className="shopping">
+                <p>Also available On</p>
+            </div>
+            <div className="brand-icons">
+                <img src="/images/amazon.png" alt="amazonsLogo" />
+                <img src="/images/flipkart.png" alt="flipCartlogo" />
+            </div>
+        </div>
+        <div className="hero-image">
+            <img src="images/sbdunk.webp" alt="sbdunk" />
+
+        </div>
+    </main>
+    );
+};
+export default SbDunk
